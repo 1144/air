c65385b1ed1d76fce7264fb80efa2bc6b49a5daa
hahaboy <flm1144@163.com>
2015-03-20 11:51:44 +0800

    tiny change

--- test-node-air.js
+++ test-node-air.js
@@ -1,8 +1,8 @@
 
 	var air = require('air-js');
 	var byteLength = air.use('byteLength'); //使用模块简称引用模块
-	console.log(byteLength('中国人'));
+	console.log(byteLength('中国人')); // 6
 
 	console.log(air.use('air/string/byteLength')('中国人民')); //使用模块全称引用模块
 
-	console.log(air.use('thousand')(78934.25));
+	console.log(air.use('thousandFloat')(78934.25)); // 78,934.25
